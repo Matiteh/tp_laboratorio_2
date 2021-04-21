b6a12343587085963b38f0808970c27c292ab2be
Matiteh <47353468+Matiteh@users.noreply.github.com>
2021-04-21 16:42:03 -0300

    Add files via upload

---
+++ TP1_Calculadora_Molina/Entidades/Calculadora.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        private static string validarOperador(string operador)
+        {
+            if((operador!="+")&& (operador != "-") && (operador != "*") && (operador != "/"))
+            {
+                operador = "+";
+            }
+            return operador;
+        }
+        public static double operar(Numero num1, Numero num2, string op)
+        {
+            double result = 0;
+            switch (validarOperador(op))
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}

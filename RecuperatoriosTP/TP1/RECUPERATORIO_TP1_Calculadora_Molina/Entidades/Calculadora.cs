ddcb87563acaf2093c4d7d147ccf137c68bdb31b
Matiteh <47353468+Matiteh@users.noreply.github.com>
2021-05-11 22:25:34 -0300

    Add files via upload

---
+++ RecuperatoriosTP/TP1/RECUPERATORIO_TP1_Calculadora_Molina/Entidades/Calculadora.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        private static string validarOperador(string operador)
+        {
+            if ((operador != "+") && (operador != "-") && (operador != "*") && (operador != "/"))
+            {
+                operador = "+";
+            }
+            return operador;
+        }
+        public static double operar(Numero num1, Numero num2, string op)
+        {
+            double result = 0;
+            switch (validarOperador(op))
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
